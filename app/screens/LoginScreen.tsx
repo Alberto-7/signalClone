e1b71e5155d565ab182bd062ed2ad3d68d18c88e
Alberto <afernandez1697@gmail.com>
2024-04-22 11:58:32 -0400

    add folder for separate screens

---
+++ app/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LoginScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-teal-600">
+      <Text>LoginScreen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
