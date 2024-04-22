44716e9d2c46b5679aeec9559553f351fcc56153
Alberto <afernandez1697@gmail.com>
2024-04-22 11:22:13 -0400

    add firebase dep and file

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSdpiZlO3cd2Zb34Q13QVjGkKKzV06zyM",
+  authDomain: "signalclone-ab64f.firebaseapp.com",
+  projectId: "signalclone-ab64f",
+  storageBucket: "signalclone-ab64f.appspot.com",
+  messagingSenderId: "1073354081870",
+  appId: "1:1073354081870:web:5aa1b023cc674674c7651f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
