852454141b5f3b480494a2a2184290f7e79af702
Alberto <afernandez1697@gmail.com>
2024-05-02 22:48:04 -0400

    implement register screen with sign in/out and redirection. Home Screen started

--- app/(screens)/LoginScreen.tsx
+++ app/(screens)/LoginScreen.tsx
@@ -1,53 +1,80 @@
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { Button, Input } from "@rneui/base";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { KeyboardAvoidingView } from "react-native";
+import { router } from "expo-router";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth, signInWithEmailAndPassword } from "@/firebase";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [paswrd, setPaswrd] = useState("");
 
-  const signIn = () => {};
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, () => {
+      console.log(auth.currentUser);
+      if (auth.currentUser) {
+        router.replace("/(screens)/HomeScreen");
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, paswrd).catch((error) =>
+      alert(error)
+    );
+  };
 
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      className="flex-1 items-center justify-start p-10 bg-white"
-    >
-      <StatusBar style="light" />
-      <Image
-        style={{ height: 200, width: 200 }}
-        source={{
-          uri: "https://images.vexels.com/media/users/3/299488/isolated/preview/8c8c1857cbcf222280a12a7f5a122abc-message-chat-bubbles-technology-icon.png",
-        }}
-      />
-      <View className="w-300">
-        <Input
-          placeholder="Email"
-          autoFocus
-          value={email}
-          onChangeText={(text) => setEmail(text)}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior="padding"
+        className="flex-1 items-center justify-start p-10 bg-white"
+      >
+        <StatusBar style="light" />
+        <Image
+          style={{ height: 200, width: 200 }}
+          source={{
+            uri: "https://images.vexels.com/media/users/3/299488/isolated/preview/8c8c1857cbcf222280a12a7f5a122abc-message-chat-bubbles-technology-icon.png",
+          }}
         />
-        <Input
-          placeholder="Password"
-          secureTextEntry
-          value={paswrd}
-          onChangeText={(text) => setPaswrd(text)}
+        <View style={{ width: 300 }}>
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Input
+            placeholder="Password"
+            secureTextEntry
+            value={paswrd}
+            onChangeText={(text) => setPaswrd(text)}
+            onSubmitEditing={signIn}
+          />
+        </View>
+
+        <Button
+          containerStyle={{ width: 200, marginTop: 10 }}
+          onPress={signIn}
+          title="Login"
         />
-      </View>
-      <Button
-        containerStyle={{ width: 200, marginTop: 10 }}
-        onPress={signIn}
-        title="Login"
-      />
-      <Button
-        containerStyle={{ width: 200, marginTop: 10 }}
-        type="outline"
-        title="Register"
-      />
-      <View className="h-100" />
-    </KeyboardAvoidingView>
+        <Button
+          containerStyle={{ width: 200, marginTop: 10 }}
+          onPress={() => router.push("/(screens)/RegisterScreen")}
+          type="outline"
+          title="Register"
+        />
+        <View className="h-100" />
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
