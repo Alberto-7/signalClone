33b510be458f58db1f45621b2b79c8e7aee47eda
Alberto <afernandez1697@gmail.com>
2024-05-02 22:46:30 -0400

    add firebase initialization. add component folder + Stack.Screens

--- firebase.js
+++ firebase.js
@@ -1,5 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,6 +20,21 @@ const firebaseConfig = {
   messagingSenderId: "1073354081870",
   appId: "1:1073354081870:web:5aa1b023cc674674c7651f",
 };
-
+const alreadyCreatedApps = getApps();
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app =
+  alreadyCreatedApps.length === 0
+    ? initializeApp(firebaseConfig)
+    : alreadyCreatedApps[0];
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export {
+  db,
+  auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+};
